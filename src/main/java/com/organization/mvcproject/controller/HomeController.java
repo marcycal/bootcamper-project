59d907a3df8c2ce50d9ad2a7827bd32b4cbeddfb
marcy.calderon <marcy.calderon@AFSWW107BYACAE.us.accenturefederal.com>
2021-05-12 15:46:26 -0500

    Changed home.html to home.jsp Changed contents of home.jsp Changed homecontroller

--- src/main/java/com/organization/mvcproject/controller/HomeController.java
+++ src/main/java/com/organization/mvcproject/controller/HomeController.java
@@ -1,18 +1,27 @@
 package com.organization.mvcproject.controller;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
 @Controller
 public class HomeController {
-
-	@RequestMapping(value="/hello")
-	public ModelAndView hello(@RequestParam(required=false, defaultValue="World") String name) {
-		ModelAndView ret = new ModelAndView("html/home");
-		// Adds an objet to be used in home.jsp
-		ret.addObject("name", name);
-		return ret;
-	}
+    @RequestMapping(value="/hello")
+    public ModelAndView hello(@RequestParam(required=false, defaultValue="World") String name) {
+        ModelAndView ret = new ModelAndView("home");
+        // Adds an object to be used in home.jsp        
+        ret.addObject("name", testingMethod());
+        practiceLoop();
+        return ret;
+    }
+    public String testingMethod() {
+        return "testing testingMethod()";
+    }
+    public void practiceLoop() {
+        String testString = "";
+        for(int i=0; i<=10; i++) {
+            System.out.println("i: "+i);
+            testString += i;
+            System.out.println("testString: " + testString);
+        }
+    }
 }
